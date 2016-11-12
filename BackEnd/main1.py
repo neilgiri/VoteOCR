459c60027ec49e8605db2e96d95c2b53e5b1f97a
Yuan-Cheng Tsai <yuancheng.tsai@berkeley.edu>
2016-11-12 03:00:08 -0800

    Organized and Comments

--- BackEnd/main1.py
+++ BackEnd/main1.py
@@ -1,9 +1,24 @@
 import pytesseract
 from PIL import Image, ImageEnhance, ImageFilter
 
-image = Image.open(r'File_000.jpeg')
-image = image.filter(ImageFilter.MedianFilter())
-enhance = ImageEnhance.Contrast(image)
-image = enhance.enhance(2)
-image = image.convert('1')
+image = Image.open(r'Top_half.png')
+image = image.filter(ImageFilter.MinFilter(1))
+
+#Increasing Contrast
+contrast = ImageEnhance.Contrast(image)
+image = contrast.enhance(50)
+
+#Making Picture Black+White
+color = ImageEnhance.Color(image)
+image = color.enhance(0.0)
+
+#Increasing Brightness
+bright = ImageEnhance.Brightness(image)
+image = bright.enhance(5.0)
+
+#Increasing Sharpness
+sharpness = ImageEnhance.Sharpness(image)
+image = sharpness.enhance(5.0)
+
+#Printing translation
 print(pytesseract.image_to_string(image))

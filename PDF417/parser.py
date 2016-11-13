8920298d174c30cd8f0872736dab6307e64d09f9
Yuan-Cheng Tsai <yuancheng.tsai@berkeley.edu>
2016-11-12 16:41:02 -0800

    Modified parser.py to take in argument

--- PDF417/parser.py
+++ PDF417/parser.py
@@ -1,22 +1,25 @@
+import sys
 from dateutil.relativedelta import relativedelta
 import datetime
 
-writer = open('sampleOUT.txt', 'w')
-codes = ["DAC", "DAD", "DCS", "DBB", "DAG", "DAI", "DAJ", "DAK"]
 
-with open('sampleIN.txt') as f:
-    for line in f:
-        code = line[0:3]
-        if code in codes:
-            writer.write(line[3:])
-        elif code == 'DBC':
-            if line[3] == '1':
-                writer.write("MALE\n")
-            else:
-                writer.write("FEMALE\n")
-        elif code == 'DDJ':
-            dateT = datetime.date(int(line[7:]), 
-                    int(line[3:5]), int(line[5:7]))
-            writer.write(
-                    (dateT - relativedelta(years=3)).strftime('%m%d%Y'))
-writer.close()
+def main(filename):
+    writer = open('OUT.txt', 'w')
+    codes = ["DAC", "DAD", "DCS", "DBB", "DAG", "DAI", "DAJ", "DAK"]
+
+    with open(filename) as f:
+        for line in f:
+            code = line[0:3]
+            if code in codes:
+                writer.write(line[3:])
+            elif code == 'DBC':
+                if line[3] == '1':
+                    writer.write("MALE\n")
+                else:
+                    writer.write("FEMALE\n")
+            elif code == 'DDJ':
+                dateT = datetime.date(int(line[7:]), 
+                        int(line[3:5]), int(line[5:7]))
+                writer.write(
+                        (dateT - relativedelta(years=3)).strftime('%m%d%Y'))
+    writer.close()

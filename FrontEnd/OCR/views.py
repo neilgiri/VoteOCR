26c0c9f7ef3b9fe5c3c36cacd144f28c6c20b985
Ryan Lund <ryan.lund@berkeley.edu>
2016-11-13 04:34:18 -0800

    Further UI work

--- FrontEnd/OCR/views.py
+++ FrontEnd/OCR/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from .forms import Voter, GetImage
 from PIL import *
 from .register import main
+#from .file import crawler
 
 readValues = {}
 def Thanks(request):
@@ -28,7 +29,7 @@ def VoterForm(request):
             d['lang'] = f.cleaned_data['langpref']
             #d['county'] = getCounty()
             d['ppp'] = f.cleaned_data['ppp']
-            print(d)
+            #Script(d)
             return HttpResponseRedirect('/Thanks/')
     else:
         f = Voter(initial=readValues, label_suffix='')
